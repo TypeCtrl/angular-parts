6780f5c82e0866f41d8ea160d33d7bd6edf07453
Scott Cooper <scttcper@gmail.com>
2018-03-20 22:42:29 -0700

    fix: adjust card content

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -4,11 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styles: [`
-  .input-group-text {
+  .input-group-prepend > .input-group-text {
     border-right: none;
   }
   .form-control {
     border-left-width: 0;
+    border-right-width: 0;
   }
   `]
 })
